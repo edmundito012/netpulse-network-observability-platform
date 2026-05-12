e33bd9b333cc0c10a0542ef2278b9a4b1e844bd5
Edmundo Albarracin <edmundoh.albarracinb@gmail.com>
2026-05-12 19:58:12 +0200

    feat(db): configure PostgreSQL database connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,27 @@
 from fastapi import FastAPI
+from app.db.session import engine
+from app.models.base import Base
 
 app = FastAPI(
     title="NetPulse API",
     description="Network Observability Platform API",
     version="0.1.0"
 )
+
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
+
+
 @app.get("/")
 def root():
-    return{
+    return {
         "app": "NetPulse",
         "status": "running",
         "docs": "/docs"
     }
+
 
 @app.get("/health")
 def health():
